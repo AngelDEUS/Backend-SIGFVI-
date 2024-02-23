4503f20f5e2b6cbdd384fcf740270a59f0af8eeb
Angel Ayala <m44p1angel@gmail.com>
2024-02-23 13:26:32 -0500

    Se agrega el componente modal y se crean nuevas funciones para el Pedido, como agragar y actualizar en MODAL.

--- SIGFVI (VI) - Clone/SIGFVI (BackEnds)/BackendVentas/src/index.js
+++ SIGFVI (VI) - Clone/SIGFVI (BackEnds)/BackendVentas/src/index.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+/*
+    * Index principal.
+*/
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Exportaciones de modulos y dependencias necesarios para el BackEnd
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-reportWebVitals();
+// ---> Modulos de Ventas y Facturación:
+const pedidosRouter = require('./routers/pedidosRouter');
+
+
+// - Uses
+const app = express();
+const PORT = process.env.PORT || 3001;
+// - Cors Options
+const optionsCors = {
+    origin: `http://localhost:3000`,
+    methods: 'GET, POST, PUT, DELETE',
+    optionsSuccessStatus: 200,  
+};
+app.use(cors(optionsCors)); // - Use de las opciones inicializadas del cors.
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// - Rutas principales --->
+app.use('/', pedidosRouter);
+app.get("/", (req, res) => { // Mensajes de pagina principal.
+    res.send("¡Hola! Este es el servidor backend!");
+    console.log("¡Hola! Este es el servidor backend!");
+});
+
+
+// - Listen del puerto.
+app.listen(PORT, ()=>{
+    console.log(`\n\n     El servidor funcionando en el puerto: \x1b[33m[${PORT}]\x1b[33m.`);
+    console.log(`\n     Local:                  http://localhost:${PORT}\x1b[0m\n`);
+});
--- SIGFVI (VI) - Clone/SIGFVI (BackEnds)/BackendVentas/src/pages-views/mod_Informes/main_informes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const main_informes = () => {
-    return (
-        <div>main_informes</div>
-    )
-}
-
-export default main_informes
